9f63da92f07641c3cf787584afc01b6db43134d6
Anne Franklin <anmerfrank@gmail.com>
2019-11-12 00:23:02 +0000

    exercise 6 completed

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,7 +6,7 @@ puts "----------"
 
 # Your code goes here ...
 
-@store1 = Store.find_by(name: "Burnaby")
+@store1 = Store.find_by(id: 1)
 @store2 = Store.find_by(id: 2)
 
 @store1.name = "Kamloops"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Jane", last_name: "Austen", hourly_rate: 60)
+@store1.employees.create(first_name: "Max", last_name: "Power", hourly_rate: 200)
+@store1.employees.create(first_name: "Darth", last_name: "Vader", hourly_rate: 80)
+
+@store2.employees.create(first_name: "Tree", last_name: "Frog", hourly_rate: 80)
+@store2.employees.create(first_name: "Stock", last_name: "Employee", hourly_rate: 80)
+@store2.employees.create(first_name: "Walter", last_name: "White", hourly_rate: 80)
